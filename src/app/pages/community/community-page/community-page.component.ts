8bd9dbf17010d9ea24ba695dfcfd7bb4404650e3
Alexis Pineda <ixspnd.ap@gmail.com>
2020-11-06 20:04:25 +0800

    Connected homepage and community page to data service

--- src/app/pages/community/community-page/community-page.component.ts
+++ src/app/pages/community/community-page/community-page.component.ts
@@ -6,7 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./community-page.component.scss']
 })
 export class CommunityPageComponent implements OnInit {
-
+  communityItems: Array<any> = [
+    {
+      id: 0,
+      title: "aaa",
+      users: 13
+    },
+    {
+      id: 1,
+      title: "bbb",
+      users: 13
+    },
+    {
+      id: 2,
+      title: "ccc",
+      users: 13
+    },
+    {
+      id: 3,
+      title: "ddd",
+      users: 13
+    },
+  ]
+  
   constructor() { }
 
   ngOnInit(): void {
