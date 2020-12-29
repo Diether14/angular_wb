0f8e0c40586ff196265709cb9bc91cd693465edc
shiroyuki28 <tampos.emmanuel@gmail.com>
2020-12-29 22:16:39 +0800

    updated user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private baseUrl = "http://localhost:3414/api/v1/auth/"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  login =  (namespace, data) => {
+    return  this.http.post(`${this.baseUrl}${namespace}`, data)
+  }
 }
