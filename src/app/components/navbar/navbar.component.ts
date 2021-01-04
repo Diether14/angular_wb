c2171e7a3e8a4c6a9783a916905071a44cbd651c
shiroyuki28 <tampos.emmanuel@gmail.com>
2021-01-04 22:16:26 +0800

    updated navbar.component.ts added logout function

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FaIconLibrary } from "@fortawesome/angular-fontawesome";
 import { faBell, faSmile, faUserCircle } from "@fortawesome/free-regular-svg-icons";
 import { faSearch, faHome, faUser, faCog, faPenSquare, faUserFriends } from "@fortawesome/free-solid-svg-icons";
+import { UserService } from '../../services/user.service'
 
 @Component({
   selector: 'nodaq-navbar',
@@ -12,7 +13,7 @@ export class NavbarComponent implements OnInit {
   public isMenuBarCollapsed: boolean = true
   isLogin:boolean=false;
 
-  constructor(iconLibrary: FaIconLibrary) {
+  constructor(iconLibrary: FaIconLibrary,private us: UserService,) {
     iconLibrary.addIcons(
       faHome,
       faBell,
@@ -28,5 +29,11 @@ export class NavbarComponent implements OnInit {
   }
   ngOnInit(): void {
   }
+  logout(){
+    this.us.logout('logout',document.cookie['nodaqsession']).subscribe((res:any)=>{
+      console.log(res)
+    })
+    console.log("logout")
+    }
 
 }
