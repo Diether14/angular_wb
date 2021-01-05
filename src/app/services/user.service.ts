bfeead45e3ce35d1be3bff95898a248d37f87afe
shiroyuki28 <tampos.emmanuel@gmail.com>
2021-01-05 22:25:53 +0800

    updated user.service.ts

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,20 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
-
+import { CookieService } from 'ngx-cookie-service'
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   private baseUrl = "http://localhost:3414/api/v1/auth/"
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private cookie:CookieService) { }
   login =  (namespace, data) => {
-    return  this.http.post(`${this.baseUrl}${namespace}`, data)
+
+    return  this.http.post(`${this.baseUrl}${namespace}`, data,{withCredentials:true})
     
     // return  this.http.post<any>(`${this.baseUrl}${namespace}`, data,{headers:{'Cookie':document.cookie},withCredentials:true})
   }
   logout =  (namespace,data) => {
-    return  this.http.post(`${this.baseUrl}${namespace}`,data)
+    console.log()
+    this.cookie.delete('sess')
+    return  this.http.post(`${this.baseUrl}${namespace}`,data,{withCredentials:true})
+
   }
 
   
