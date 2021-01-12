3d36f24a1c70ff086d914c8a387857205af84ab8
shiroyuki28 <tampos.emmanuel@gmail.com>
2021-01-12 19:49:36 +0800

    updated websocket.service.ts

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -8,10 +8,14 @@ export const WS_ENDPOINT = environment.wsEndpoint;
 
 const ws =  webSocket(WS_ENDPOINT);
 ws.subscribe(
-  msg => console.log(msg),
-  err => console.log(err), 
-  () => console.log('complete') 
+  msg => {
+    console.log(msg)
+    return msg;
+  },
+  err => console.log(err),
+  ()=>ws.subscribe()
 );
+// ws.subscribe()
 
 
 @Injectable({
@@ -20,19 +24,20 @@ ws.subscribe(
 
 export class WebsocketService {
 test:any =null;
-  public connect(test){
-    this.getWebSocket().subscribe(
-      msg => {
-        this.test= msg[1]
-        console.log(msg[1])
-      },
-      err => console.log(err), 
-      () => console.log('complete') 
-    )
-    console.log(this.getWebSocket())
-  }
+  // public connect(test){
+  //   this.getWebSocket().subscribe(
+  //     msg => {
+  //       this.test= msg[1]
+  //       console.log(msg[1])
+  //     },
+  //     err => console.log(err), 
+  //     () => console.log('complete') 
+  //   )
+  //   console.log(this.getWebSocket())
+  // }
   public sendMessage(msg){
     return this.getWebSocket().next(msg);
+    
   }
   
   public getWebSocket(){
